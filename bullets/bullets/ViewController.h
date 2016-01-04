e660aeb47b2aea904ad9af5abfaeebf93c782c94
TonySongWD <tonysong185@gmail.com>
2016-01-04 10:53:58 +0800

    Add the project

---
+++ bullets/bullets/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  bullets
+//
+//  Created by appledev098 on 15/10/26.
+//  Copyright © 2015年 PwC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class TTTAttributedLabel;
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet TTTAttributedLabel *contentLabel;
+
+
+@end
+
